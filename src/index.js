5f7e2b4ed7a0c005fbfb00250f4f2a782e0e2d46
blase147 <solarmails2@gmail.com>
2022-10-15 22:38:06 +0100

    Update files

--- src/index.js
+++ src/index.js
@@ -1,110 +1,79 @@
-import './style.css';
+import './index.css';
 
-const todoList = document.getElementById('todoList');
-const addListForm = document.querySelector('#form1');
-const textInput = document.getElementById('textInput');
+const { addTask, removeTask, removeAllCompleted } = require('./modules/CRUD.js');
+const { updateStatus, editDescription } = require('./modules/interactivity.js');
 
-let todos = JSON.parse(localStorage.getItem('todos'));
+const tasksList = document.querySelector('ul');
+const addTaskField = document.querySelector('.new-task-field input');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-// display task function
-const displayTask = () => {
-  let li = '';
-  if (todos) {
-    todos.forEach((todo) => {
-      const completed = todo.completed ? 'checked' : '';
-      li += `
-      <div class="checkContainer">
-      <div class="inputContainer">
-          <input id="checkBox" type="checkbox"  ${completed} data-check="${todo.index}">
-          <input id="label" data-desc="${todo.index}" value="${todo.description}">
-      </div>
-      <div class="btnContainer">
-          <button><i id="${todo.index}" class="fa fa-ellipsis-v btnDelete"></i></button>    
-      </div>
-    </div>`;
+const populateTasks = () => {
+  tasksList.innerHTML = '';
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasksList.innerHTML += `<li class="task-item task-item${tasks[i].index}">
+        <div class="task-info"><input type="checkbox" name=""><input class="task task${tasks[i].index}" type="text" placeholder="${tasks[i].description}"></div>
+        <div class="dots">
+          <i class="fa fa-ellipsis-v remove" aria-hidden="true" id="i${tasks[i].index}"></i>
+        </div>
+      </li>`;
+  }
+
+  const inputs = document.querySelectorAll('.task');
+  const checks = document.querySelectorAll('[type="checkbox"]');
+  const deleteButton = document.querySelectorAll('.remove');
+  const clearAll = document.querySelector('.clearAll');
+
+  for (let i = 0; i < tasks.length; i += 1) {
+    checks[i].checked = tasks[i].completed;
+    if (checks[i].checked) {
+      inputs[i].classList.add('strike');
+    } else {
+      inputs[i].classList.remove('strike');
+    }
+    checks[i].addEventListener('click', () => { // eslint-disable-line
+      updateStatus(checks[i], tasks[i], inputs[i]);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      populateTasks();
+    });
+
+    clearAll.addEventListener('click', () => { // eslint-disable-line
+      tasks = removeAllCompleted(tasks);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      populateTasks();
+    });
+
+    inputs[i].addEventListener('focusin', () => { // eslint-disable-line
+      deleteButton[i].classList.remove('fa-ellipsis-v');
+      deleteButton[i].classList.add('fa-trash');
+      deleteButton[i].addEventListener('click', (e) => {
+        const indexToDelete = e.target.id[1];
+        removeTask(indexToDelete, tasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        populateTasks();
+      });
+    });
+
+    inputs[i].addEventListener('input', (e) => { // eslint-disable-line
+      editDescription(e.target, tasks[i]);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    });
+
+    inputs[i].addEventListener('focusout', (e) => {
+      e.target.value = '';
+      deleteButton[i].classList.add('fa-ellipsis-v');
+      deleteButton[i].classList.remove('fa-trash');
     });
   }
-  todoList.innerHTML = li;
-};
-displayTask();
-
-// add task
-const addList = (e) => {
-  e.preventDefault();
-  const userInput = textInput.value.trim();
-  textInput.value = '';
-  if (!userInput) return;
-  if (!todos) {
-    todos = [];
-  }
-  const list = {
-    description: userInput,
-    completed: false,
-    index: todos.length + 1,
-  };
-  todos.push(list);
-  localStorage.setItem('todos', JSON.stringify(todos));
-  displayTask();
-};
-addListForm.addEventListener('submit', addList);
-
-// remove task
-const removeList = (index) => {
-  const newlist = todos.filter((element) => element.index !== index);
-  todos.length = 0;
-  let i = 1;
-  newlist.forEach((element) => {
-    element.index = i;
-    i += 1;
-  });
-  todos.push(...newlist);
-  localStorage.setItem('todos', JSON.stringify(todos));
-  displayTask();
 };
 
-todoList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('btnDelete')) {
-    const index = parseInt(e.target.getAttribute('id'), 10);
-    removeList(index);
+addTaskField.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    addTask(addTaskField.value, tasks);
+    addTaskField.value = '';
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    populateTasks();
   }
 });
 
-// update task
-const editList = (e) => {
-  const clicked = e.target.closest('#label');
-  if (!clicked) return;
-  clicked.addEventListener('keyup', () => {
-    const taskList = JSON.parse(localStorage.getItem('todos')) || [];
-    const targetList = parseInt(clicked.getAttribute('data-desc'), 10);
-    const update = taskList.find((todo) => todo.index === targetList);
-    update.description = clicked.value.trim();
-    localStorage.setItem('todos', JSON.stringify(taskList));
-  });
-};
-todoList.addEventListener('click', editList);
-
-const clearList = (e) => {
-  const clicked = e.target.closest('#checkBox');
-  if (!clicked) return;
-  const targetList = parseInt(clicked.getAttribute('data-check'), 10);
-  const taskList = JSON.parse(localStorage.getItem('todos')) || [];
-  const update = taskList.find((todo) => todo.index === targetList);
-  update.completed = !update.completed;
-  localStorage.setItem('todos', JSON.stringify(taskList));
-};
-
-const clearListBtn = document.querySelector('.clearListBtn');
-clearListBtn.addEventListener('click', () => {
-  const clearAll = JSON.parse(localStorage.getItem('todos')) || [];
-  const notCompleted = clearAll.filter((todo) => !todo.completed);
-  todos.length = 0;
-  let i = 0;
-  notCompleted.forEach((element) => {
-    element.index = i;
-    i += 1;
-  });
-  todos.push(...notCompleted);
-  localStorage.setItem('todos', JSON.stringify(todos));
-  displayTask();
-});
-todoList.addEventListener('click', clearList);
+populateTasks();
